5e945e61045766ad7a7af7a5c9d4c35847b538d9
Adam Armour <adam.armour@teamaol.com>
2016-12-01 20:54:53 -0700

    change title

--- src/root.js
+++ src/root.js
@@ -11,7 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var RootComponent = (function () {
     function RootComponent() {
-        this.title = 'ngPoland';
+        this.title = 'MapQuest';
     }
     return RootComponent;
 }());
--- src/root.ts
+++ src/root.ts
@@ -1,13 +1,13 @@
 import {Component} from '@angular/core'
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-root',
-    templateUrl: './root.html',
-    styleUrls: ['./root.css'],
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: './root.html',
+  styleUrls: ['./root.css'],
 })
 export class RootComponent {
 
-    title: String = 'ngPoland';
+  title: String = 'MapQuest';
 
 }
