b14271cc6877cdd40603e482a83a403c7f86dd8f
Adam Armour <adam.armour@teamaol.com>
2016-12-03 17:48:45 -0700

    set map center and zoom

--- src/map/map.component.js
+++ src/map/map.component.js
@@ -16,7 +16,9 @@ var MapComponent = (function () {
     MapComponent.prototype.ngAfterViewInit = function () {
         this.map = new mapboxgl.Map({
             container: 'map',
-            style: 'https://api.mapbox.com/styles/v1/mapquest/ciuike7qg000i2inyel1id74o?access_token=pk.eyJ1IjoibWFwcXVlc3QiLCJhIjoiY2Q2N2RlMmNhY2NiZTRkMzlmZjJmZDk0NWU0ZGJlNTMifQ.mPRiEubbajc6a5y9ISgydg'
+            style: 'https://api.mapbox.com/styles/v1/mapquest/ciuike7qg000i2inyel1id74o?access_token=pk.eyJ1IjoibWFwcXVlc3QiLCJhIjoiY2Q2N2RlMmNhY2NiZTRkMzlmZjJmZDk0NWU0ZGJlNTMifQ.mPRiEubbajc6a5y9ISgydg',
+            center: [-104.999575, 39.750392],
+            zoom: 14
         });
     };
     return MapComponent;
